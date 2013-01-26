7cd7fd47a59892c469d649bcca1da2cd0356589e
touho <gilder@gilderBook.local>
2013-01-26 04:33:31 +0200

    box in level

--- js/level.js
+++ js/level.js
@@ -1,3 +1,43 @@
-function Level() {
-	box2d.create.circle({r:1, x:9, y:7, static:true});
+function Level(game) {
+
+	this.bodies = [];
+
+	//Border
+	for (var a = 0; a < Math.PI*2; a += Math.PI/40) {
+		var x = 7 + Math.sin(a)*10;
+		var y = 7 + Math.cos(a)*10;
+		var r = 1 + Math.random();
+		var body = box2d.create.circle({r:r, x:x, y:y, static:true});
+		body.r = r;
+		this.bodies.push(body);
+	}
+
+	//Inner
+	for (var a = 0; a < Math.PI*2; a += Math.PI/10) {
+		var x = 7 + Math.sin(a)*1;
+		var y = 7 + Math.cos(a)*1;
+		var r = 0.5 + Math.random();
+		var body = box2d.create.circle({r:r, x:x, y:y, static:true});
+		body.r = r;
+		this.bodies.push(body);
+	}
+}
+
+Level.prototype.draw = function(context) {
+
+	/*
+	context.fillStyle = "red";
+	for (var i = 0; i < this.bodies.length; i++) {
+		var body = this.bodies[i];
+		var x = body.m_xf.position.x;
+		var y = body.m_xf.position.y;
+		context.fillRect(x-body.r, y-body.r, body.r*2, body.r*2);
+	}
+	*/
+
+	for (var i = 0; i < this.bodies.length; i++) {
+		var body = this.bodies[i];
+		//body.m_linearVelocity.x = 1;
+	}
+
 }
