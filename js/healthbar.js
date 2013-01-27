eb962f26b4cbe66f20304d9185bfb8220dc589f1
viljamipeltola <webengineer@viljamipeltola.com>
2013-01-27 08:22:27 +0200

    Health bar graphics Layer on the canvas re organized (not yet removed from javascript)

--- js/healthbar.js
+++ js/healthbar.js
@@ -14,7 +14,7 @@ function Healthbar( o ) {
 	this.p = o.p;
 	
 	function bar(x,y,w,h,c,id) {
-		return '<div id="' + id + '" style="position:fixed;top:' + y + 'px;left:' + x + 'px; width:' + w + 'px; height: ' + h + 'px; background-color:' + c + ';"></div>';
+		return '<div id="' + id + '" style="position:fixed;top:' + y + 'px;left:' + x + 'px; width:' + w + 'px; height: ' + h + 'px; background-color:' + c + ';background-image:url(\'img/energy_bg.png\')"></div>';
 	}
 	this.barContainer = $(bar(this.x,this.y,this.w,this.h,'#ff0000','barContainer')).appendTo(document.body);
 	this.healthbar = $(bar(this.x,this.y,this.w * this.p ,this.h,'#00ff00','healthbar')).appendTo(document.body);	
